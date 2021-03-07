2d3b79a06664f4f879362132f701a3645c1132cf
SoF <wdj930729@naver.com>
2021-03-07 12:51:28 +0900

    Update CF_1296C.py

--- CF_1296C.py
+++ CF_1296C.py
@@ -8,8 +8,8 @@ for j in range(t):
     x = y = 0
     d = {}
     d[(x,y)] = 0
-    start = -1
-    end = n
+    l = -1
+    r = n
     for i in range(n):
         if arr[i] == 'R':
             y+=1
@@ -20,11 +20,11 @@ for j in range(t):
         elif arr[i] == 'D':
             x-=1
         if (x,y) in d:
-            if i-d[(x,y)] < end-start:
-                end = i
-                start = d[(x,y)]
+            if i-d[(x,y)] < r-l:
+                r= i
+                l = d[(x,y)]
         d[(x,y)] = i+1
-    if start == -1:
+    if l == -1:
         print(-1)
     else:
-        print(start+1,end+1)
+        print(l+1,r+1)
