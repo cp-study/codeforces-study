fd739ffd177f5fad4db0b5ab4e07f7bf82642500
SoF <wdj930729@naver.com>
2021-05-02 12:04:43 +0900

    Update CF_1514C.py

--- CF_1514C.py
+++ CF_1514C.py
@@ -2,18 +2,12 @@ from math import gcd
  
 n = int(input())
 ans = []
-check = []
 prod = 1
 for i in range(1, n):
        if (gcd(n, i) == 1):
               prod = prod*i%n
-              check.append(prod)
               ans.append(i)
-while True:
-    if check[-1] != 1:
-        ans.pop()
-        check.pop()
-    else:
-        break
+if prod != 1:
+       ans.pop()
 print(len(ans))
 print(*ans)
