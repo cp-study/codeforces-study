b7ca11d9b33a73a4392cab48751c8dba1543cd2d
SoF <wdj930729@naver.com>
2021-05-09 12:21:51 +0900

    Update CF_1169B.py

--- CF_1169B.py
+++ CF_1169B.py
@@ -1,26 +1,59 @@
-ans = "NO"
 n,m = (((map(int, input().split()))))
 arr = []
 for i in range(m):
     a,b = (((map(int, input().split()))))
     arr.append([a,b])
  
+flag = False
 x = arr[0][0]
-y = [i for i in range(n+1)]
-y = set(y)
+brr = []
 for pair in arr:
     if x not in pair:
-        y = y.intersection(set(pair))
-if len(y)>0:
-    ans="YES"
- 
+        brr.append(pair)
+if len(brr) == 0:
+    print("YES")
+    exit()
+Dict = {}
+for pair in brr:
+    if pair[0] not in Dict:
+        Dict[pair[0]] = 1
+    else:
+        Dict[pair[0]] +=1
+    if pair[1] not in Dict:
+        Dict[pair[1]] = 1
+    else:
+        Dict[pair[1]] +=1
+for i in Dict:
+    if Dict[i] == len(brr):
+        flag = True
+        break
+if flag:
+    print("YES")
+    exit()
+
 x = arr[0][1]
-y = [i for i in range(n+1)]
-y = set(y)
+brr = []
 for pair in arr:
     if x not in pair:
-        y = y.intersection(set(pair))
-if len(y)>0:
-    ans="YES"
- 
-print(ans)
+        brr.append(pair)
+if len(brr) == 0:
+    print("YES")
+    exit()
+Dict = {}
+for pair in brr:
+    if pair[0] not in Dict:
+        Dict[pair[0]] = 1
+    else:
+        Dict[pair[0]] +=1
+    if pair[1] not in Dict:
+        Dict[pair[1]] = 1
+    else:
+        Dict[pair[1]] +=1
+for i in Dict:
+    if Dict[i] == len(brr):
+        flag = True
+        break
+if flag:
+    print("YES")
+else:
+    print("NO")
